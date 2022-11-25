01447149a3d2f80d6d6b34ef712e82238d432e14
sakshimatiray <113632675+sakshimatiray@users.noreply.github.com>
2022-11-25 16:28:15 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+const element=document.querySelector('#root');
+const root = ReactDOMcreateRoot(element);
 
-
+root.render(<App />);
 ReactDOM.render(<App />, document.getElementById("root"));
